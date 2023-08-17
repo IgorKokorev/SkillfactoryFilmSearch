577f42898316fcc7ad9988b661a1965a301bbb33
Igor Kokorev <ikokorev@mail.ru>
2023-08-17 23:35:39 +0200

    Сделано задание 24 урока: 1. Вставлен RecyclerView со списком фильмов. Карточка каждого фильма реализована с помощью двух вложенных LinearLayout, ConstraintLayout в данном случае глючил. 2. Добавлена реализация DiffUtils. 3. Добавлена активити с деталями фильма. Реализована с помощью CoordinatorView. 4. На страницу деталей добавлено нижнее меню со снекбарами при нажатии на кнопки. 5. Добавлен снекбар при нажатии на FAB.

---
+++ app/src/main/java/film/search/filmsearch/Film.kt
@@ -0,0 +1,31 @@
+package film.search.filmsearch
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Film(
+    val poster: Int,
+    val title: String,
+    val description: String
+) : Parcelable {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Film
+
+        if (poster != other.poster) return false
+        if (title != other.title) return false
+        if (description != other.description) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = poster
+        result = 31 * result + title.hashCode()
+        result = 31 * result + description.hashCode()
+        return result
+    }
+}
---
+++ app/src/main/java/film/search/filmsearch/FilmViewHolder.kt
@@ -0,0 +1,8 @@
+package film.search.filmsearch
+
+import androidx.recyclerview.widget.RecyclerView
+import film.search.filmssearch.databinding.FilmItemBinding
+
+class FilmViewHolder(val binding: FilmItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+}
