6a9c4349d43764577a1e9da6840acee4487f0480
IgorKokorev <kokorev.igor@gmail.com>
2023-10-30 11:40:32 +0100

    Homework 33 Transfer to MVVM pattern: 1. Organized project structure; 2. Films DB moved to Repository class; 3. Added Interactor class to access the repo; 4. Added ViewModels for 2 fragments: Main & Favorite; 5. Performed refactoring of both fragments; 6. Added App class for global access to common constants and variables.

--- app/src/main/java/film/search/filmsearch/App.kt
+++ app/src/main/java/film/search/filmsearch/App.kt
@@ -1,12 +1,22 @@
 package film.search.filmsearch
 
 import android.app.Application
+import androidx.lifecycle.ViewModelProvider.NewInstanceFactory.Companion.instance
 import film.search.filmsearch.data.MainRepository
 import film.search.filmsearch.domain.Interactor
+import film.search.filmsearch.utils.NotificationService
 
 class App : Application() {
     lateinit var repo: MainRepository
     lateinit var interactor: Interactor
+    lateinit var notificationService: NotificationService
+    val TRANSITION_NAME = "transition"
+    val TIME_INTERVAL = 2000L
+    val FILM = "film"
+    val POSITION = "position"
+    val POSTER = "poster"
+    val DESCRIPTION = "description"
+    val FRAGMENT_TAG = "tag"
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/film/search/filmsearch/view/MainActivity.kt
+++ app/src/main/java/film/search/filmsearch/view/MainActivity.kt
@@ -3,6 +3,7 @@ package film.search.filmsearch.view
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import film.search.filmsearch.App
 import film.search.filmsearch.domain.Film
 import film.search.filmsearch.utils.NotificationService
 import film.search.filmsearch.view.fragments.CollectionFragment
@@ -19,25 +20,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var backPressed = 0L
 
-    // Contains constants:
-    // interval for double tap in ms
-    // name of the bundle with film info to transfer to another fragment
-    // allFilms - fake film db
-    // favoriteFilms - list of favorite films
-    companion object {
-        const val TIME_INTERVAL = 2000L
-        const val FILM = "film"
-        const val POSITION = "position"
-        const val TRANSITION_NAME = "transition"
-        const val POSTER = "poster"
-        const val DESCRIPTION = "description"
-        const val FRAGMENT_TAG = "tag"
-        lateinit var notificationService: NotificationService
-//        var allFilms = mutableListOf<Film>()
-//        val favoriteFilms = mutableListOf<Film>()
-//        val filmDetailsFragment = FilmDetailsFragment()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -48,26 +30,21 @@ class MainActivity : AppCompatActivity() {
         // initializing menu buttons click listeners
         initMenuButtons()
 
-        // initializing fake films db
-//        initFilmsDB()
-
         // initializing notification service
-        notificationService = NotificationService(this)
-
-//        filmDetailsFragment.sharedElementEnterTransition = AutoTransition().setDuration(1000L)
+        App.instance.notificationService = NotificationService(this)
 
         // starting main fragment
         supportFragmentManager
             .beginTransaction()
             .add(R.id.fragment_placeholder, MainFragment())
-            .addToBackStack(FRAGMENT_TAG)
+            .addToBackStack(App.instance.FRAGMENT_TAG)
             .commit()
     }
 
     // Check back pressed. If 2 times in less than 2 sec om main screen - exit. If not main screen (fragment) - back.
     override fun onBackPressed() {
         if (supportFragmentManager.backStackEntryCount <= 1) {
-            if (backPressed + TIME_INTERVAL > System.currentTimeMillis()) {
+            if (backPressed + App.instance.TIME_INTERVAL > System.currentTimeMillis()) {
                 onBackPressedDispatcher.onBackPressed()
                 finish()
             } else {
@@ -102,7 +79,7 @@ class MainActivity : AppCompatActivity() {
                     supportFragmentManager
                         .beginTransaction()
                         .replace(R.id.fragment_placeholder, fragment, tag)
-                        .addToBackStack(FRAGMENT_TAG)
+                        .addToBackStack(App.instance.FRAGMENT_TAG)
                         .commit()
                     true
                 }
@@ -112,7 +89,7 @@ class MainActivity : AppCompatActivity() {
                     supportFragmentManager
                         .beginTransaction()
                         .replace(R.id.fragment_placeholder, fragment, tag)
-                        .addToBackStack(FRAGMENT_TAG)
+                        .addToBackStack(App.instance.FRAGMENT_TAG)
                         .commit()
                     true
                 }
@@ -123,7 +100,7 @@ class MainActivity : AppCompatActivity() {
                     supportFragmentManager
                         .beginTransaction()
                         .replace(R.id.fragment_placeholder, fragment, tag)
-                        .addToBackStack(FRAGMENT_TAG)
+                        .addToBackStack(App.instance.FRAGMENT_TAG)
                         .commit()
                     true
                 }
@@ -134,7 +111,7 @@ class MainActivity : AppCompatActivity() {
                     supportFragmentManager
                         .beginTransaction()
                         .replace(R.id.fragment_placeholder, fragment, tag)
-                        .addToBackStack(FRAGMENT_TAG)
+                        .addToBackStack(App.instance.FRAGMENT_TAG)
                         .commit()
                     true
                 }
@@ -147,15 +124,15 @@ class MainActivity : AppCompatActivity() {
     // Launch Film Details screen (fragment) and transfer film data to it
     fun launchDetailsFragment(film: Film, position: Int, filmItemBinding: FilmItemBinding) {
         val bundle = Bundle()
-        bundle.putParcelable(FILM, film)
-        bundle.putInt(POSITION, position)
+        bundle.putParcelable(App.instance.FILM, film)
+        bundle.putInt(App.instance.POSITION, position)
         val fragment = FilmDetailsFragment()
         fragment.arguments = bundle
 
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.fragment_placeholder, fragment)
-            .addToBackStack(FRAGMENT_TAG)
+            .addToBackStack(App.instance.FRAGMENT_TAG)
             .commit()
     }
 
