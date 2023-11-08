7cc1a036208b38afe4a4a11dc057c4a74053ac85
IgorKokorev <kokorev.igor@gmail.com>
2023-11-08 12:38:12 +0100

    Updated dependencies

--- app/src/main/java/film/search/filmsearch/viewmodel/MainFragmentViewModel.kt
+++ app/src/main/java/film/search/filmsearch/viewmodel/MainFragmentViewModel.kt
@@ -2,7 +2,6 @@ package film.search.filmsearch.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.bumptech.glide.Glide.init
 import film.search.filmsearch.App
 import film.search.filmsearch.domain.Film
 import film.search.filmsearch.domain.Interactor
@@ -14,5 +13,40 @@ class MainFragmentViewModel : ViewModel() {
     init {
         val films = interactor.getFilmsDB()
         filmsListLiveData.postValue(films)
+
+//        val client = OkHttpClient()
+//
+//        val request1 = Request.Builder()
+//            .url("https://api.themoviedb.org/3/authentication")
+//            .get()
+//            .addHeader("accept", "application/json")
+//            .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDc4NzQzNGYzMzg1NjcyYWVlYTA4MDAzMzRjMWUxNyIsInN1YiI6IjY1NDRkNGE3MWFjMjkyMDExYjNkNzgzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PLydGoD3-dmqbnBIPWQanSIlWBfNby_BJIkKGMO49_0")
+//            .build()
+//
+//        val response1 = client.newCall(request1).enqueue(object : Callback {
+//            override fun onFailure(call: Call, e: IOException) {
+//                e.printStackTrace()
+//            }
+//            override fun onResponse(call: Call, response: Response) {
+//                try {
+//                    val responseBody = response.body
+//                    println("!!! ${responseBody?.string()}")
+//                } catch (e: Exception) {
+//                    println("!!! $response")
+//                    e.printStackTrace()
+//                }
+//            }
+//        })
+//
+//
+//        val request2 = Request.Builder()
+//            .url("https://api.themoviedb.org/3/search/movie?query=weaks&include_adult=true&language=en-US&page=1")
+//            .get()
+//            .addHeader("accept", "application/json")
+//            .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDc4NzQzNGYzMzg1NjcyYWVlYTA4MDAzMzRjMWUxNyIsInN1YiI6IjY1NDRkNGE3MWFjMjkyMDExYjNkNzgzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PLydGoD3-dmqbnBIPWQanSIlWBfNby_BJIkKGMO49_0")
+//            .build()
+//
+//        val response2 = client.newCall(request2).execute()
+//        response2.body
     }
 }
